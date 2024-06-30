c037ac29b825273bdc67040bf07df096df412887
MrJay 0xJ <71602701+mrjaycodes@users.noreply.github.com>
2024-06-30 07:49:32 +0330

    main.h

---
+++ StuduntManagmentSys/main.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include <random>
+
+using namespace std;
+
+struct School
+{
+	class teacher
+	{
+		int t_id;
+		int t_name;
+	public:
+		void AddToDb();
+	};
+};
